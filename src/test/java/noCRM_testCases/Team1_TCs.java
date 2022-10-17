8ea711e58eb015f1f03a8b606ff14ca4d4bd62bb
federicormn <federico.roman@nybblegroup.com>
2022-10-17 17:07:08 -0300

    ADD --> Setup method & Test method

--- src/test/java/noCRM_testCases/Team1_TCs.java
+++ src/test/java/noCRM_testCases/Team1_TCs.java
@@ -1,6 +1,36 @@
 package noCRM_testCases;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Team1_TCs
 {
+    WebDriver driver;
 
+    @BeforeEach
+    public void setup()
+    {
+
+
+        String mySo = System.getProperty("os.name");
+
+        if(mySo.equals("Windows 10"))
+        {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.get("https://todo.ly/");
+        }else
+        {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver");
+            driver = new ChromeDriver();
+            driver.get("https://todo.ly/");
+        }
+    }
+    @Test
+    public void test()
+    {
+        System.out.println("no rompe");
+    }
 }
